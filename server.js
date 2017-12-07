dfe8ada558ba8e2d9aa958ecf10964777c5292ec
daviddxu <daviddxu@cmail.carleton.ca>
2017-12-06 20:58:25 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var  app = express(); //create express middleware dispatcher
+
+const PORT = process.env.PORT || 3000
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs'); //use hbs handlebars wrapper
+
+app.locals.pretty = true; //to generate pretty view-source code in browser
+
+//read routes modules
+var routes = require('./routes/index');
+
+//some logger middleware functions
+function methodLogger(request, response, next){           
+		   console.log("METHOD LOGGER");
+		   console.log("================================");
+		   console.log("METHOD: " + request.method);
+		   console.log("URL:" + request.url);
+		   next(); //call next middleware registered
+}
+function headerLogger(request, response, next){           
+		   console.log("HEADER LOGGER:")
+		   console.log("Headers:")
+           for(k in request.headers) console.log(k);
+		   next(); //call next middleware registered
+}
+
+//register middleware with dispatcher
+//ORDER MATTERS HERE
+//middleware
+app.use(routes.authenticate); //authenticate user
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
